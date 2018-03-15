96d5ec24efdee9945756e560e961b752b8f38850
ihoou <yinhao527@sina.com>
2018-03-15 16:44:38 +0800

    优化登录验证请求

--- njdtmaster/njdtmaster/WelcomeViewController.swift
+++ njdtmaster/njdtmaster/WelcomeViewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 import AdSupport
+import Alamofire
+import SwiftyJSON
 
 
 class WelcomeViewController: UIViewController{
     
-    
+    var loginFlag:String = "2"
+    var loginMsg:String = ""
     
     //在控制器定义全局的可变data，用户存储接收的数据
     var jsonData:NSMutableData = NSMutableData()
@@ -22,23 +25,29 @@ class WelcomeViewController: UIViewController{
         
         var uuid:String!
         uuid = ASIdentifierManager.shared().advertisingIdentifier.uuidString
-//        print("batteryLevel=\(UIDevice.current.batteryLevel)")
-//        print(ssss.replacingOccurrences(of: "-", with: ""))
-//        print("localizedModel=\(UIDevice.current.localizedModel)")
-//        print("model=\(UIDevice.current.model)")
-//        print("name=\(UIDevice.current.name)")
-//        print("systemName=\(UIDevice.current.systemName)")
-//        print("systemVersion=\(UIDevice.current.systemVersion)")
         //调用登陆接口
-        let md = NSMutableDictionary()
+//        let md = NSMutableDictionary()
         uuid = uuid.replacingOccurrences(of: "-", with: "")
-//        print("UUID=\(uuid)")
-        md.setValue("{\"txcode\":\"login\",\"imei\":\"\(uuid as String)\"}", forKey: "data")
-        
-        let a = BaseNetwork()
-        var result:String = "Null"
-        a.post(url: "http://njdt.njtjy.org.cn:10011/njliftMaint/android/process.action", params: md)
-        
+        Alamofire.request(CommonData.CONSTANT_PATH_POST, method: .post, parameters: ["data":"{\"txcode\":\"login\",\"imei\":\"\(uuid as String)\"}"], encoding: URLEncoding.default, headers: nil).responseJSON { (response) in
+            
+            switch response.result.isSuccess {
+            case true:
+                if let value = response.result.value {
+                    //print(登陆验证反馈信息：\(value))
+                     let json = JSON(value)
+                    CommonData.COMPANY_NAME = json["data"]["maintName"].string!
+                    CommonData.USER_NAME = json["data"]["staffName"].string!
+                    CommonData.TERMINAL_IDENTIFICATION = json["data"]["staffImei"].string!
+                    CommonData.LAST_LOGIN_TIME = json["data"]["lastTime"].string!
+                    self.loginFlag = json["rescode"].string!
+                    self.loginMsg = json["msg"].string!
+                }else{
+                    print(response.result.value!)
+                }
+            case false:
+                print("调用登陆接口发生错误")
+            }
+        }
         
         let time1: TimeInterval = 0.3
         DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + time1) {
@@ -49,33 +58,26 @@ class WelcomeViewController: UIViewController{
         
         let time: TimeInterval = 2.3
         DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + time) {
-            //code
             //print("2 秒后输出")
-            result = a.getReturnValue()
-            //print(result)
-            let dic:NSDictionary = self.getDictionaryFromJSONString(jsonString: result)
-//            for (key,value) in dic {
-//                print("键：\(key),值：\(value)")
-//            }
-            let loginflag:String = dic["rescode"]! as! String
-            if loginflag == "1" {
-                print("登陆失败，原因是：\(dic["msg"]!)")//登陆失败，已注册但未认领，跳转认领提示页面
+            if self.loginFlag == "1" {
+                print("登陆失败，原因是：\(self.loginMsg)")//登陆失败，已注册但未认领，跳转认领提示页面
                 let toast = ToastView()
                 toast.showToast(text: "已经注册！请联系公司管理员认领！", pos: .Bottom)
-                
+
                 let time: TimeInterval = 3//延迟
                 DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + time) {
                     exit(0)//使用该方法，上架苹果商店可能被拒。
                 }
-                
-            }else if loginflag == "0"{
-                print("登陆成功，原因是：\(dic["msg"]!)")
+
+            }else if self.loginFlag == "0"{
+                print("登陆成功，原因是：\(self.loginMsg)")
                 //登陆成功，跳转至主页面
 //                self.performSegue(withIdentifier: "validatesuccess", sender: nil)
 //                 self.present(MainTabViewController(), animated:true, completion:nil)
+                //加载主页面
                 (UIApplication.shared.delegate as! AppDelegate).loadMainView()
-            }else if loginflag == "2"{
-                print("登陆失败，原因是：\(dic["msg"]!)")
+            }else if self.loginFlag == "2"{
+                print("登陆失败，原因是：\(self.loginMsg)")
                 //登陆失败，将跳转至注册页面
                 self.performSegue(withIdentifier: "validatefaild", sender: nil)
             }
